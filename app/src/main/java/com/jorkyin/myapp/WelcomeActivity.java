93424f86433057d60011e2bcbb2f8c39c7956d78
jorkyin <429129585@qq.com>
2016-03-01 21:15:59 +0800

    添加WelcomeActivity

---
+++ app/src/main/java/com/jorkyin/myapp/WelcomeActivity.java
@@ -0,0 +1,15 @@
+package com.jorkyin.myapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by YinJian on 2016/3/1.
+ */
+public class WelcomeActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
