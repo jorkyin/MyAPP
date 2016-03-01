88fcb1543ef9eea62be4c31685fe923184ed5343
jorkyin <jorkyin@163.com>
2016-03-01 23:27:17 +0800

    修改Welcome到MainActivity自动跳转，跳转时间为3s；

--- app/src/main/java/com/jorkyin/myapp/WelcomeActivity.java
+++ app/src/main/java/com/jorkyin/myapp/WelcomeActivity.java
@@ -1,15 +1,26 @@
 package com.jorkyin.myapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 
 /**
  * Created by YinJian on 2016/3/1.
  */
 public class WelcomeActivity extends AppCompatActivity{
+    private Handler mHandler = new Handler();
+    private Intent mIntent ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mIntent =new Intent(WelcomeActivity.this,MainActivity.class);
+                startActivity(mIntent);
+            }
+        },3000);
     }
 }
