a88deba7c43e7119d8af71b17d300150af0fd897
jorkyin <jorkyin@163.com>
2016-03-31 00:44:23 +0800

    更新

--- app/src/main/java/com/jorkyin/myapp/MainActivity.java
+++ app/src/main/java/com/jorkyin/myapp/MainActivity.java
@@ -1,12 +1,20 @@
 package com.jorkyin.myapp;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.AssetFileDescriptor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.MediaPlayer;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
+import android.webkit.WebView;
 
 import com.jorkyin.myapp.broadcast.SendBrocastActivity;
-import com.jorkyin.myapp.broadcast.TestBroadcastReceiver;
 import com.jorkyin.myapp.girdviewdemo.GridViewDemo;
 import com.jorkyin.myapp.listViewDemo.ListViewDemo;
 import com.jorkyin.myapp.myFragment.TestFragmentActivity;
@@ -14,6 +22,12 @@ import com.jorkyin.myapp.myHandler.TestHandlerActivity;
 import com.jorkyin.myapp.myview.TestRedButtonActivity;
 import com.jorkyin.myapp.scrolDemo.ScrolViewDemo;
 import com.jorkyin.myapp.servers.MusicActivity;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -23,6 +37,109 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         initActivity();
+       // testFileDemo();
+        //testAssets();
+       // testResFile();
+
+       // testSDCard();
+    }
+
+    private void testSDCard() {
+        //获取SD卡目录
+        String tempFile = Environment.getExternalStorageDirectory().getAbsolutePath();
+        Environment.getDataDirectory();//获取Android中的打他数据目录
+        Environment.getDownloadCacheDirectory();
+        Environment.getExternalStorageDirectory();
+    }
+
+    private void testResFile() {
+        //读资源文件
+        //InputStream inputStreams = getResources().openRawResource(R.raw.LIbai);
+        //读取资源文件
+        getResources().getColor(R.color.colorAccent);
+    }
+
+    private void testAssets() {
+        //Write file in the assets
+        WebView webView = new WebView(this);
+        webView.loadUrl("file:///android_asset/Test.html");
+
+        try {
+            //Read file   open只能是文件不是是文件夹
+            InputStream inputStream = getResources().getAssets().open("Test.html");
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
+        try {
+            //读取文件名列表
+            String[] fileNames = getAssets().list("images/");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            //读取数据流
+            InputStream inputStream = getAssets().open("images/dog.ipg");
+            //蒋数据转换为图片（位图）
+            Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //读音频
+        try {
+            AssetFileDescriptor assetFileDescriptora = getAssets().openFd("libai.mp3");
+            MediaPlayer mediaPlayer = new MediaPlayer();
+            mediaPlayer.reset();
+            mediaPlayer.setDataSource(assetFileDescriptora.getFileDescriptor(),
+                    assetFileDescriptora.getStartOffset(),
+                    assetFileDescriptora.getLength());
+            mediaPlayer.prepare();
+            mediaPlayer.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void testFileDemo() {
+        String string = "Create a new file of Test.txt in the internal storage";
+        //Create a new file of Test.txt in the internal storage
+        File file = new File(getFilesDir(), "Test.txt");
+        Log.i("MainActivity", "getFilesDir:" + getFilesDir().getAbsolutePath());
+        Log.i("MainActivity", "file:" + file.getAbsolutePath());
+
+        try {
+            //Create new Test.txt of file;
+            boolean isSuccess = file.createNewFile();
+
+        } catch (IOException e) {
+            Log.i("MainActivity", "Test.txt create error!");
+            e.printStackTrace();
+        }
+
+        try {
+            //Create new file of test2.txt for FileOutputStream fun;
+            FileOutputStream fileOutputStream = openFileOutput("test2.txt", Context.MODE_PRIVATE);
+            try {
+                //Write data to test2.txt file
+                fileOutputStream.write(string.getBytes());
+                //Close file
+                fileOutputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        //Check external storage state
+        String state = Environment.getExternalStorageState();
+        if (TextUtils.equals(state, Environment.MEDIA_MOUNTED)) {
+
+        }
     }
 
     private void initActivity() {
--- app/src/main/java/com/jorkyin/myapp/listViewDemo/ListViewDemo.java
+++ app/src/main/java/com/jorkyin/myapp/listViewDemo/ListViewDemo.java
@@ -2,7 +2,6 @@ package com.jorkyin.myapp.listViewDemo;
 
 import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
